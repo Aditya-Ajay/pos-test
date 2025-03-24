5c182d374fef3161910d90b1f3c8b2580c1b3f64
Aditya <aditya.k@solistechnology.in>
2025-03-24 13:37:15 +0530

    done

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -14,8 +14,9 @@ import downArrowIcon from '../assets/downarrow.png';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { fetchOverviewAnalytics } from "../redux/Analytics/analyticsSlice";
 import { fetchTopCustomers } from '../redux/Customer/CustomerSlice'
+import { fetchOverviewAnalytics } from "../redux/Analytics/analyticsSlice";
+
 function StatCard({ icon: Icon, label, value, subValue, trend, growthPercentage  }) {
   return (
     <div className="bg-white rounded-lg w-[100%] h-[119px] p-[12px]" style={{ border: '1px solid #EEEEEF' }}>
