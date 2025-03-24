09a1cb3ec14f398d61f1940c3025ed4c0f2dca9c
Aditya <aditya.k@solistechnology.in>
2025-03-24 13:41:52 +0530

    done

--- src/components/SimpleAreaCharts.jsx
+++ src/components/SimpleAreaCharts.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchRevenueAnalytics } from "../redux/Analytics/analyticsSlice";
+import { fetchRevenueAnalytics } from "../redux/Analytics/AnalyticsSlice";
 import {
   AreaChart,
   Area,
