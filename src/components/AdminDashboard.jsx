6a6707319a8e42d3a784ffb53ae13fc4eb1aacd5
Shivam Aggarwal <shivam@solistechnology.in>
2025-03-24 12:57:16 +0530

    revert

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -14,10 +14,8 @@ import downArrowIcon from '../assets/downarrow.png';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-// import { fetchOverviewAnalytics } from "../redux/Analytics/analyticsSlice.jsx";
-import { fetchOverviewAnalytics } from '@/redux/Analytics/AnalyticsSlice';
-import { fetchTopCustomers } from '@/redux/Customer/CustomerSlice';
-// import { fetchTopCustomers } from '../redux/Customer/CustomerSlice.jsx'
+import { fetchOverviewAnalytics } from "../redux/Analytics/analyticsSlice.jsx";
+import { fetchTopCustomers } from '../redux/Customer/CustomerSlice'
 function StatCard({ icon: Icon, label, value, subValue, trend, growthPercentage  }) {
   return (
     <div className="bg-white rounded-lg w-[100%] h-[119px] p-[12px]" style={{ border: '1px solid #EEEEEF' }}>
